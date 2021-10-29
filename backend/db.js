d6c546feedc2fb384aa320a38c6108bf1e7d33f8
nimit05 <59357629+nimit05@users.noreply.github.com>
2021-10-29 23:00:58 +0530

    minor change

--- backend/db.js
+++ backend/db.js
@@ -38,45 +38,45 @@ const AvailableSeats = db.define("availableSeats" , {
 })
 
 // // Inserting values in table
-// AvailableSeats.sync().then(async() => {
-//   let i = 1;
-//   while(i <= 11){
-//     try {
-//       await AvailableSeats.create({
-//         RowNum: i,
-//         Seats: 7
-//       });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//     i++
-//   }
+AvailableSeats.sync().then(async() => {
+  let i = 1;
+  while(i <= 11){
+    try {
+      await AvailableSeats.create({
+        RowNum: i,
+        Seats: 7
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    i++
+  }
 
-//   await AvailableSeats.create({
-//     RowNum: 12,
-//     Seats: 3
-//   });
+  await AvailableSeats.create({
+    RowNum: 12,
+    Seats: 3
+  });
 
-// })
+})
 
-// Seats.sync().then(async() => {
-//   let i = 1;
-//   let pos = i;
-//   while(i <= 80){
-//     try {
-//       pos = parseInt(i/7) + (i%7 == 0 ? 0 : 1);
-//       await Seats.create({
-//         SeatNum: parseInt(i),
-//         RowNum: pos
-//       });
-//     } catch (error) {
-//       console.log(error)
-//       break;
-//     }
-//     i++;
-//   }
+Seats.sync().then(async() => {
+  let i = 1;
+  let pos = i;
+  while(i <= 80){
+    try {
+      pos = parseInt(i/7) + (i%7 == 0 ? 0 : 1);
+      await Seats.create({
+        SeatNum: parseInt(i),
+        RowNum: pos
+      });
+    } catch (error) {
+      console.log(error)
+      break;
+    }
+    i++;
+  }
  
-// });
+});
 
 module.exports =  {
   Seats,
